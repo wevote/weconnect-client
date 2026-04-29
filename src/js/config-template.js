bdd81a420cf8b06700b0c63809d255720ff0f7d2
Joe Evans <joseph.evans@gmail.com>
2026-04-29 12:20:15 -0700

    Use https for local development backend server by default.

--- src/js/config-template.js
+++ src/js/config-template.js
@@ -15,10 +15,10 @@ module.exports = {
   ///////////////////////////////////////////////
   // Keep all three configuration blocks below, but only uncomment one of them at a time.
   //// Connecting to local WeConnect "weconnect-server" APIs ////
-  STAFF_API_SERVER_ROOT_URL: 'http://localhost:4500/',
-  STAFF_API_SERVER_ADMIN_ROOT_URL: 'http://localhost:4500/admin/',
-  STAFF_API_SERVER_API_ROOT_URL: 'http://localhost:4500/apis/v1/',
-  STAFF_API_SERVER_API_CDN_ROOT_URL: 'http://localhost:4500/apis/v1/',
+  STAFF_API_SERVER_ROOT_URL: 'https://localhost:4500/',
+  STAFF_API_SERVER_ADMIN_ROOT_URL: 'https://localhost:4500/admin/',
+  STAFF_API_SERVER_API_ROOT_URL: 'https://localhost:4500/apis/v1/',
+  STAFF_API_SERVER_API_CDN_ROOT_URL: 'https://localhost:4500/apis/v1/',
   //// weconnect-server local server running SSL/TLS HTTPS
   // STAFF_API_SERVER_ROOT_URL: 'https://wevotedeveloper.com:4500/',
   // STAFF_API_SERVER_ADMIN_ROOT_URL: 'https://wevotedeveloper.com:4500/admin/',
